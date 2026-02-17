776aa7c5958f504679fac782c672a8915755a23e
JunaidMokit <junaid.rahman.mokit096@gmail.com>
2026-02-18 04:04:54 +0600

    re start

--- main.go
+++ main.go
@@ -3,21 +3,8 @@ package main
 import "fmt"
 
 func main() {
-   // fmt.Println("Hello, World!")
-   var x int =10
-   y:=20
-   sum:=x+y
-   fmt.Println("Result is=",sum)
+   
 
-   if sum>20 {
-    fmt.Println("Perfact Sum")
-   } else if sum <20 && sum >15 {
-    fmt.Println("Moderate Sum")
-   } else {
-    fmt.Println("Not Good")
-   }
+fmt.Println("Hello,World")
 
-   for i:=0;i<5;i++ {
-    fmt.Println(i)
-   }
 }
