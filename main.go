3bca7a0bd1ffac7683997f3d4d95eb1430a5c95b
JunaidMokit <junaid.rahman.mokit096@gmail.com>
2026-03-17 03:56:12 +0600

    learn function

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import "fmt"
 
+func add(num1 int, num2 int){
+	sum:=num1+num2
+	fmt.Println(sum)
+}
+
 func main() {
    
 
-mark:=20
+// mark:=20
 
-if mark>50{
-	fmt.Println("You are passed with good mark")
-} else if mark>33{
-	fmt.Println("You are passed ")
-} else{
-	fmt.Println("You are not eligible")
-}
+// if mark>50{
+// 	fmt.Println("You are passed with good mark")
+// } else if mark>33{
+// 	fmt.Println("You are passed ")
+// } else{
+// 	fmt.Println("You are not eligible")
+// }
+add(3,5)
 
 }
